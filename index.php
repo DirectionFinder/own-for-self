cc83979546ff93c673f7546fdfbb04e5c972ccf3
Ivan <ip.web.df@gmail.com>
2019-10-27 21:21:02 +0200

    #3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "<!DOCTYPE HTML> <html><head><title>git!</title></head><body><h1>GIT LESSONS</h1></body></html>";
-echo "<!DOCTYPE HTML> <html><head><title>git!</title></head><body><h1>GIT LESSONS</h1></body></html>";
+echo "<!DOCTYPE HTML> <html><head><title>NO!!</title></head><body><h1>GIT LESSONS</h1></body></html>";
 echo "<!DOCTYPE HTML> <html><head><title>git!</title></head><body><h1>GIT LESSONS</h1></body></html>";
